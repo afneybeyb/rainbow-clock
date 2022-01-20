e9580a3a65cfde39537363538669e33df7a83084
afneybeyb <afneybeyb@gmail.com>
2022-01-20 23:49:08 +0100

    background color generates aswell

--- script.js
+++ script.js
@@ -22,37 +22,47 @@ function displayTime() {
 	let time = [date.getHours().toString(), date.getMinutes().toString(), date.getSeconds().toString()]
 	for (let i = 0; i < time.length; i++) {
 		if (time[i].length != 2) {
-			time[i] = ("0" + time[i]);
+			time[i] = "0" + time[i];
 		}
 		document.getElementById("time_" + ((i + 1) * 2 - 1)).innerHTML = time[i].slice(0, 1);
 		document.getElementById("time_" + ((i + 1) * 2)).innerHTML = time[i].slice(1, 2);
 	}
 	if (time[2] % 12 == 0) {
-		changeStyle();
+		changeClockColor();
 	}
 	setTimeout(displayTime, 1000);
 }
 
-function changeStyle() {
+function genColor(min, max) {
+	let color = "";
+	for (let i = 0; i < 3; i++) {
+		let part = Math.floor(min + Math.random() * max).toString(16);
+		if (part.length != 2) {
+			part = "0" + part;
+		}
+		color += part;
+	}
+	return "#" + color;
+}
+
+function changeClockColor() {
 	let time = [];
 	for (let i = 0; i < 6; i++) {
 		time.push(document.getElementById("time_" + (i + 1)));
 	}
 	time.forEach(element => {
-		let color = "";
-		for (let i = 0; i < 3; i++) {
-			color += Math.floor(0x77 + Math.random() * 0x88).toString(16);
-		}
-		element.style.color = "#" + color;
+		element.style.color = genColor(0x77, 0x88);
 	});
+}
 
-	/* 	bgColor += Math.floor(0x77 + Math.random() * 0x88).toString(16);
-	
-		document.getElementById("content").style.background = "linear-gradient(0deg, #" + color + ", #646464)"
-	 */
+function changeBackground() {
+	let color = genColor(0x00, 0x33);
+	let deg = Math.random() * 180;
+	document.getElementById("content").style.background = "linear-gradient(" + deg + "deg, " + color + ", #202020)";
 }
 
 buildTime();
 displayTime();
-changeStyle();
+changeClockColor();
+changeBackground();
 
